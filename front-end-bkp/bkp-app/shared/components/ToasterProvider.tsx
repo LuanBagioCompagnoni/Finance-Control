f4dc7b5f759987cbdb8ad5d642bf5b33d843db79
Luan Bagio Compagnoni <luancmpg@gmail.com>
2026-05-04 17:45:33 -0300

    feat: improve auth page and register

--- front-end-bkp/bkp-app/shared/components/ToasterProvider.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import React from 'react';
-
-import Toaster from './toaster';
-
-export default function ToasterProvider() {
-  return <Toaster />;
-}
--- front-end-bkp/bkp-app/shared/components/icons/types/index.ts
+++
@@ -1,3 +0,0 @@
-import { JSX, SVGProps } from 'react';
-
-export type IconProps = JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>;
